c188eee6d9385b51d0852c7a872990fcd52f1427
walmho <jones.ryan.t.01@gmail.com>
2022-02-16 11:38:45 -0800

    Deleted unneeded test file, commented on file structure

--- datagather.py
+++ datagather.py
@@ -1,3 +1,5 @@
-from readfrom import x
-
-print(x)
+"""
+Purpose of this file is to be the actual scraper that gathers data from sites and inputs
+into variables to be used in main.py for final analysis and display with matplotlib. file will
+intake requested data from init.py and be used in main.py (I hope)
+""" 
