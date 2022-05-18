b95e5f387a8aaa581772bc5e1711f8ade670ba21
walmho <jones.ryan.t.01@gmail.com>
2022-05-18 11:38:39 -0700

    Finished base logins for each user-friendly function, going to plug them into the GUI and main.py file before extracting data

--- datagather.py
+++ datagather.py
@@ -52,8 +52,8 @@ def studentVUE_UF(username, password):
     driver.get("https://myvue.cascadetech.org/hillsboro/PXP2_Login_Student.aspx?regenerateSessionId=True")
     
     #logging in
-    userLogin = driver.find_element_by_id("ctl00_MainContent_username")
-    userLogin.send_keys(username)
+    studentLogin = driver.find_element_by_id("ctl00_MainContent_username")
+    studentLogin.send_keys(username)
     passwordLogin = driver.find_element_by_id("ctl00_MainContent_password")
     passwordLogin.send_keys(password)
     
@@ -61,8 +61,25 @@ def studentVUE_UF(username, password):
     nextButton.click()
     
 def emailCheck_UF(username, password):
-    pass
+    #Same intuitive issue with studentVUE
+    driver.get("https://outlook.office.com/mail/inbox")
+    time.sleep(3)
+    #logging in
+    emailLogin = driver.find_element_by_id("i0116")
+    emailLogin.send_keys(username)
+    nextButton = driver.find_element_by_id("idSIButton9")
+    nextButton.click()
+    time.sleep(3)
+    
+    passwordEntry = driver.find_element_by_id("i0118")
+    passwordEntry.send_keys(password)
+    nextButton = driver.find_element_by_id("idSIButton9")
+    nextButton.click()
+    
+    dontStayLogged = driver.find_element_by_id("idBtn_Back")
+    dontStayLogged.click()
     
 newWindow_UF()
 #googleClassroom_UF("joner689@hsd.k12.or.us", "PASSWORDWOULDBEHERE!")
-studentVUE_UF("joner689", "PASSWORDWOULDBEHERE!")
+#studentVUE_UF("joner689", "PASSWORDWOULDBEHERE!")
+emailCheck_UF("joner689@hsd.k12.or.us", "PASSWORDWOULDBEHERE!")
