77535d4037e15edcf72b7f912de2be6206c60d33
walmho <jones.ryan.t.01@gmail.com>
2022-05-24 11:23:47 -0700

    code can now run any 3 of the functions succesfully

--- main.py
+++ main.py
@@ -21,9 +21,17 @@ for i in range(2, len(userPreferences)):
 
 if init.userFriendly(selectedBrowser, version):
     if version == 1:
-        #Will need to find a way to automatically run the corresponding functions for each selection
-        if userInfo[0] == "Calculate GPA":
-            studentVUE_UF(userInfo[1], userInfo[2], webdriver.Chrome(ChromeDriverManager().install()))
+        #If there's only one selection:
+        if len(userInfo) == 3:
+            #Will need to find a way to automatically run the corresponding functions for each selection
+            #Third value passed in function needs to change to be a var so different browsers can actually be used
+            if userInfo[0] == "Calculate GPA":
+                studentVUE_UF(userInfo[1], userInfo[2], webdriver.Chrome(ChromeDriverManager().install()))
+            elif userInfo[0] == "List Assignments (Google Classroom)":
+                googleClassroom_UF(userInfo[1], userInfo[2], webdriver.Chrome(ChromeDriverManager().install()))
+            elif userInfo[0] == "Check Email":
+                emailCheck_UF(userInfo[1], userInfo[2], webdriver.Chrome(ChromeDriverManager().install()))
+
 else:
     print("Using BeautifulSoup for instant data\n")
     #" "
