53fd6fbecb5725199d8d65460244b73848c170f4
walmho <jones.ryan.t.01@gmail.com>
2022-05-26 10:23:06 -0700

    Working on making a reliable way to index userInfo list to scrape each site, regardless of the combination the user chooses. May need to re-orient the way checklist.py stores user selections

--- main.py
+++ main.py
@@ -17,20 +17,36 @@ for i in range(2, len(userPreferences)):
     #Eventually make this load into a dictionary where the key is the site needed and values are user, password
     userInfo.append(userPreferences[i])
 
-    #print(userInfo)
+print(userInfo)
 
 if init.userFriendly(selectedBrowser, version):
     if version == 1:
+        siteAmount = int(len(userInfo)/3)
+        for website in range(siteAmount):
+            #Var declaration needs to be later or else a list index error will rise
+            #username = website+1
+            #password = website+2
+            print("\n{}th iteration".format(website))
+            print(userInfo[website])
+            
+            if userInfo[website] == "Calculate GPA":
+                studentVUE_UF(userInfo[website+siteAmount], userInfo[website+siteAmount+1], webdriver.Chrome(ChromeDriverManager().install()))
+            elif userInfo[website] == "List Assignments (Google Classroom)":
+                googleClassroom_UF(userInfo[website+1], userInfo[website+siteAmount+2], webdriver.Chrome(ChromeDriverManager().install()))
+            elif userInfo[website] == "Check Email":
+                emailCheck_UF(userInfo[website+siteAmount], userInfo[website+siteAmount+1], webdriver.Chrome(ChromeDriverManager().install()))
+            
         #If there's only one selection:
-        if len(userInfo) == 3:
-            #Will need to find a way to automatically run the corresponding functions for each selection
-            #Third value passed in function needs to change to be a var so different browsers can actually be used
-            if userInfo[0] == "Calculate GPA":
-                studentVUE_UF(userInfo[1], userInfo[2], webdriver.Chrome(ChromeDriverManager().install()))
-            elif userInfo[0] == "List Assignments (Google Classroom)":
-                googleClassroom_UF(userInfo[1], userInfo[2], webdriver.Chrome(ChromeDriverManager().install()))
-            elif userInfo[0] == "Check Email":
-                emailCheck_UF(userInfo[1], userInfo[2], webdriver.Chrome(ChromeDriverManager().install()))
+        #The code below works but is commented out cause it only allows one function to be run
+        # if len(userInfo) == 3:
+        #     #Will need to find a way to automatically run the corresponding functions for each selection
+        #     #Third value passed in function needs to change to be a var so different browsers can actually be used
+        #     if userInfo[0] == "Calculate GPA":
+        #         studentVUE_UF(userInfo[1], userInfo[2], webdriver.Chrome(ChromeDriverManager().install()))
+        #     elif userInfo[0] == "List Assignments (Google Classroom)":
+        #         googleClassroom_UF(userInfo[1], userInfo[2], webdriver.Chrome(ChromeDriverManager().install()))
+        #     elif userInfo[0] == "Check Email":
+        #         emailCheck_UF(userInfo[1], userInfo[2], webdriver.Chrome(ChromeDriverManager().install()))
 
 else:
     print("Using BeautifulSoup for instant data\n")
