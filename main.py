5f17861d20196971379003d74cd8a4b9051d9ae0
walmho <jones.ryan.t.01@gmail.com>
2022-05-31 10:06:17 -0700

    currently just testing to see in what situations the program works

--- main.py
+++ main.py
@@ -36,9 +36,9 @@ compiledData = {}
 for k in range(len(newInfo)):
     compiledData[newInfo[k]] = newUser[k]
 
-# print(userInfo)
+print(userInfo)
 # print("\n\n")
-# print(compiledData)
+print(compiledData)
 # print("\n")
 
 if init.userFriendly(selectedBrowser, version):
